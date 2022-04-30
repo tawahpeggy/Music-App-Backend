79e04bd021f714b977f1aa6157831ba70a3dd95a
unknown <tawahpeggy98@gmail.com>
2022-04-30 17:05:26 +0100

    closes #1; setup environment

---
+++ index.js
@@ -0,0 +1,12 @@
+// File : index.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// PORT
+const PORT = process.env.PORT || 4000;
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
