7f345cccca09f0f8586b685015aeb789a5f49177
unknown <tawahpeggy98@gmail.com>
2022-05-03 23:53:16 +0100

    added eto'o biography to code base for reference

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// File : index.js
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-// PORT
-const PORT = process.env.PORT || 4000;
-app.get("/", (req, res) => {
-  res.json({ message: "API Working" });
-});
-app.listen(PORT, (req, res) => {
-  console.log(`Server Started at PORT ${PORT}`);
+// // File : index.js
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+// // PORT
+// const PORT = process.env.PORT || 4000;
+// app.get("/", (req, res) => {
+//   res.json({ message: "API Working" });
+// });
+// app.listen(PORT, (req, res) => {
+//   console.log(`Server Started at PORT ${PORT}`);
+// });
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
+
+// server listening 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
